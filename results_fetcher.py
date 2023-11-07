0b85c3bf35a1a45ad0b06f7d0eef38f2505ca3c1
Bojana Arsovska <b.s.arsovska@student.vu.nl>
2023-11-08 00:36:55 +0100

    Two changes were made in this vesion: 1. Table 1 use dot be counting the number of commits by author in column 1 that includes the file. This result you would get in cloumn 3. Now that has bee split into 2 tables, one that shows the edits per author plus commits including this file by that author. The other table, 1.2, shows the total code churn on the file and has removed the author's column 2. The second edit is that now code churn is calculated as lines added + deleted + edited

--- results_fetcher.py
+++ results_fetcher.py
@@ -4,10 +4,20 @@ import pandas as pd
 def get_data_from_db(cursor):
     # Execute the first query
     cursor.execute("SELECT author, file_name, SUM(changes) AS SCORE, COUNT(author) AS COUNT FROM commits GROUP BY author, file_name ORDER BY SUM(changes) DESC")
-    result1 = cursor.fetchall()
+    result1_1 = cursor.fetchall()
 
+# COUNT(*) AS COMMIT_COUNT third column, counts the
+    # number of commits for each file_name. The author column
+    # has been removed because it cannot be included without
+    # also grouping by author or using it within an aggregate
+    # function. This query will provide a list of files, the total
+    # score of changes for each file, and the total count of commits made
+    # to each file, grouped by file_name and ordered by the total score of changes (SCORE) in descending order.
+    cursor.execute("SELECT author, file_name, SUM(ABS(changes)) AS SCORE, COUNT(author) AS COUNT FROM commits GROUP BY file_name ORDER BY SUM(changes) DESC")
+    result1_2 = cursor.fetchall()
     # Convert result1 to dataframe for better view
-    df1 = pd.DataFrame(result1, columns=['Author', 'File Name', 'Number of Edits', 'Author Edits/Commits on the file '])
+    df1_1 = pd.DataFrame(result1_1, columns=['Author', 'File', 'Number of Edits by <Author>', 'Number of Commits by <Author> including<File>'])
+    df1_2 = pd.DataFrame(result1_2, columns=['Author', 'File Name', 'Code Churn - Total number of lines added/deleted/edited', 'Author Edits/Commits with the file '])
 
     # Execute the second query
     cursor.execute("""SELECT * FROM file_legacy_complexity 
@@ -18,8 +28,10 @@ def get_data_from_db(cursor):
     df2 = pd.DataFrame(result2, columns=['ID', 'File Name', 'Author', 'Contribution', 'Cognitive Complexity'] )
 
     # Save the results into CSV files
-    df1.to_csv('table1.csv', index=False)
-    df2.to_csv('table2.csv', index=False)
+    df1_1.to_csv('Table 1.1: Individual Authors Contribution to a File.csv', index=False)
+    df1_2.to_csv('Table 1.2: Total Contribution to a File.csv', index=False)
+    df2.to_csv('Table 2: Legacy and Cognitive Complexity of File.csv', index=False)
 
-    print("\nTable 1: Authors Contribution per File\n",df1)
-    print("\nTable 2: Legacy and Cognitive Complexity of Each File\n",df2)
+    print("\nTable 1.1: Individual Author's Contribution to a File\n",df1_1)
+    print("\nTable 1.2: Total Contribution to a File\n",df1_2)
+    print("\nTable 2: Legacy and Cognitive Complexity of File\n",df2)
